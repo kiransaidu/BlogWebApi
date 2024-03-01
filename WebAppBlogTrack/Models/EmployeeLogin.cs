d1f10ba9733fe6e89ac1c1d1dddd33ca347877cd
kiran <kiransaidu179@gmail.com>
2024-03-01 13:03:04 +0530

    Add project files.

---
+++ WebAppBlogTrack/Models/EmployeeLogin.cs
@@ -0,0 +1,16 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace WebAppBlogTrack.Models
+{
+    public class EmployeeLogin
+    {
+        [Required]
+        [EmailAddress]
+        public string? EmailId { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public int PassCode { get; set; }
+    }
+}
